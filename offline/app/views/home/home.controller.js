338de3b4e95aa2b05686815383247de715c027bd
mjt <thapa.manish16@gmail.com>
2017-11-13 16:51:40 +0545

    homepage

--- offline/app/views/home/home.controller.js
+++ offline/app/views/home/home.controller.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var config = require('../../../config');
+
+
 router.get('/',function (req,res) {
-	var data = { one : 'one'};
+	var list = require(config.list);
+	var data = { list : list};
 	res.render(__dirname+'/home',data)
 })
 
 module.exports = router;
+
+
