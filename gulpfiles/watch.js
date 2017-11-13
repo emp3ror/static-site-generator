30076c739d07b008ad5d494dc71af7ab2ab6e3fb
mjt <thapa.manish16@gmail.com>
2017-11-13 21:36:10 +0545

    modrewrite rule for browsersync

--- gulpfiles/watch.js
+++ gulpfiles/watch.js
@@ -1,14 +1,21 @@
 var gulp = require('gulp'),
 	watch = require('gulp-watch');
 
+	var modRewrite  = require('connect-modrewrite');
+
 var browserSync = require("browser-sync").create();
 
 
 gulp.task('watch-folder', function() {  
-     browserSync.init({
-        server: {
-            baseDir: "./dist/"
-        }
+	browserSync.init({
+		server: {
+			baseDir: "./dist/"
+		},
+		middleware: [
+		modRewrite([
+			'!\\.\\w+$ /index.html [L]'
+			])
+		]
     });
   gulp.watch('./client/**/*', ['prod']).on('change', browserSync.reload);;
 });
